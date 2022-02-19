6df230d00946a1693ed1d0a74018bc8ed244d3e3
cdelgadoh <crisdelgadohuertas@gmail.com>
2022-02-19 16:40:42 +0100

    JS realizado

--- P1/Tienda/tienda.js
+++ P1/Tienda/tienda.js
@@ -3,7 +3,6 @@
 //-- Importo los modulos http y fs
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
 
 //-- Definir el puerto a utilizar
 const Puerto = 9090;
@@ -18,19 +17,54 @@ const server = http.createServer((req, res) => {
   console.log("Petición recibida!");
 
   //-- Construir el objeto url con la url de la solicitud
-  
+  const myURL = new URL(req.url, 'http://' + req.headers['host']);
+    console.log("URL solicitada: " + myURL.pathname);
 
+    let page = '' //-- Página que queremos cargar
+    if (myURL.pathname == "/") { //-- Cuando lanzamos nuestra página web
+        page = './home.html'
+    } else if (myURL.pathname == "/ls") { // Listado con todos los ficheros de la practica
+        page = './ls.html'
+    } else { // -- En cualquier otro caso
+        page = '.' + myURL.pathname;
+    }
 
-  //-- Cabecera que indica el tipo de datos del
-  //-- cuerpo de la respuesta: Texto plano
-  res.setHeader('Content-Type', 'text/plain');
+    console.log("Página solicitada: " + page)
 
-  //-- Mensaje del cuerpo
-  res.write("Tienda on-line\n");
+    content = page.split(".").pop()
+    console.log("Contenido de la página: " + content);
 
-  //-- Terminar la respuesta y enviarla
-  res.end();
+    fs.readFile(page, (err, data) => {
+        let code = 200;
+        let code_msg = "OK";
+        let content_type = "text/html";
+
+        if (err) { // Mostramos la página de error
+            console.log('ha habido error')
+            var HOME_HTML = fs.readFileSync('./error.html', 'utf-8');
+            res.statusCode = 404;
+            res.statusMessage = "Ha habido un error";
+            res.setHeader('Content-Type', "text/html");
+            res.write(HOME_HTML);
+            return res.end();
+        }
+        switch (content) {
+            case 'html':
+                content_type = "text/html";
+            case 'css':
+                content_type = "text/css";
+            default:
+                content_type = content;
+        }
+        res.statusCode = code;
+        res.statusMessage = code_msg;
+        res.setHeader('Content-Type', content_type);
+        res.write(data);
+        res.end();
+    });
+
 });
+
 
 //-- Activar el servidor: 
 server.listen(Puerto);
