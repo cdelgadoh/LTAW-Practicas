9d754c52d04abbaa5bb2f57d8a8d8870b82c44c4
cdelgadoh <crisdelgadohuertas@gmail.com>
2022-05-04 21:12:19 +0200

    Copia de la Practica 3

---
+++ P4/client.js
@@ -0,0 +1,32 @@
+//-- Elementos del interfaz
+const display = document.getElementById("display");
+const msg_entry = document.getElementById("msg_entry");
+const send = document.getElementById("send");
+const audio = document.getElementById("audio");
+
+//-- Crear un websocket. Se establece la conexión con el servidor
+const socket = io();
+
+
+socket.on("message", (msg_entry)=>{
+  display.innerHTML += "<br> > " + msg_entry;
+  
+});
+
+send.onclick = () => {
+  if (msg_entry.value)
+    socket.send(msg_entry.value);
+    audio.play();
+  //-- Borrar el mensaje actual
+    
+}
+
+//-- Al apretar el botón se envía un mensaje al servidor
+msg_entry.onchange = () => {
+  if (msg_entry.value)
+    socket.send(msg_entry.value);
+    audio.play();
+  
+  //-- Borrar el mensaje actual
+    
+}
