9ba74191b4806eb477ccfac32f4163b99c8b3f62
cdelgadoh <crisdelgadohuertas@gmail.com>
2022-05-04 21:28:23 +0200

    Cargamos un contenido

--- P4/server.js
+++ P4/server.js
@@ -1,21 +1,24 @@
-//-- Cargar electron
+// Cargar electron
 const electron = require('selectron');
 
 console.log("Arrancando electron...");
 
-//-- Acceso a la ventana principal
+// Acceso a la ventana principal
 let win = null;
 
 //-- Punto de entrada
 electron.app.on('ready', () => {
   console.log("Evento Ready!");
 
-  //-- Creo una nueva ventana
+  // Creo una nueva ventana
   win = new electron.BrowserWindow({
-    width: 600,  //-- Anchura 
-    height: 400  //-- Altura
+    width: 600,  // Anchura 
+    height: 400  // Altura
   }); 
 
-  //-- Quitar menú por defecto
-  win.setMenuBarVisibility(false)
+  // Quitar menú por defecto
+  // win.setMenuBarVisibility(false)
+
+  // Cargar el contenido
+  win.loadURL('https://www.urjc.es/etsit');
 });
