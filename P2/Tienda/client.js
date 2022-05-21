7e3301e4a742cb01be1254cec3651a50c413da61
cdelgadoh <crisdelgadohuertas@gmail.com>
2022-05-21 14:23:18 +0200

    avances

--- P2/Tienda/client.js
+++ P2/Tienda/client.js
@@ -17,47 +17,36 @@ caja.oninput = () => {
     m.onreadystatechange = () => {
 
         //-- Petición enviada y recibida. Todo OK!
-        if (m.readyState == 4) {
+        if (m.readyState==4) {
 
             //-- Solo la procesamos si la respuesta es correcta
-            if (m.status == 200) {
+            if (m.status==200) {
 
                 //-- La respuesta es un objeto JSON
                 let productos = JSON.parse(m.responseText)
 
-                console.log("PRODUCTOOOS: " + productos);
+                console.log(productos);
 
                 //-- Borrar el resultado anterior
                 display1.innerHTML = "";
 
                 //--Recorrer los productos del objeto JSON
-                for (let i = 0; i < productos.length; i++) {
-                    let link;
-                    switch (productos[i]) {
-                        case 'Risk Star Wars':
-                            link = 'risk-detail.html';
-                            break;
-                        case 'Exploding Kittens':
-                            link = 'exploding-detail.html';
-                            break;
-                        case 'Harry Potter juego de mesa':
-                            link = 'harry-details.html';
-                            break;
-                        default:
-                            link = productos[i].toLowerCase() + '-detail.html'
-                            break;
-                    }
+                for (let i=0; i < productos.length; i++) {
 
                     //-- Añadir cada producto al párrafo de visualización
-                    display1.innerHTML += '<div class="search"><a href="' + link + '">' + productos[i] + '</a></div>';
-                    //display1.innerHTML += '<a class="search" href="' + productos[i].toLowerCase(); + '-detail.html">' + productos[i] + '</a>';
+                    display1.innerHTML += productos[i];
+
+                    //-- Separamos los productos por ',''
+                    if (i < productos.length-1) {
+                    display1.innerHTML += ', ';
+                    }
                 }
 
             } else {
                 //-- Hay un error en la petición
                 //-- Lo notificamos en la consola y en la propia web
                 console.log("Error en la petición: " + m.status + " " + m.statusText);
-                display1.innerHTML += '<p>ERROR</p>'
+                display2.innerHTML += '<p>ERROR</p>'
             }
         }
     }
@@ -65,15 +54,15 @@ caja.oninput = () => {
     console.log(caja.value.length);
 
     //-- La peticion se realia solo si hay al menos 1 carácter
-    if (caja.value.length >= 1) {
+    if (caja.value.length >= 3) {
 
-        //-- Configurar la petición
-        m.open("GET", "/productos?param1=" + caja.value, true);
+      //-- Configurar la petición
+      m.open("GET","/productos?param1=" + caja.value, true);
 
-        //-- Enviar la petición!
-        m.send();
-
+      //-- Enviar la petición!
+      m.send();
+      
     } else {
-        display1.innerHTML = "";
+        display1.innerHTML="";
     }
 }
